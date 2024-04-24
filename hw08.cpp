3bb8e7dd408cab2f927ad7175529ed521c32559a
Leonardo Herrera <leonardo.herrera1996@yahoo.com>
2024-04-24 01:32:13 -0700

    completed assignment.

--- hw08.cpp
+++ hw08.cpp
@@ -10,33 +10,84 @@ int main()
     acctOne.set_name("account One / Base class");
     acctOne.set_acct_num(550001);
     acctOne.set_balance(500.01);
+    temp = acctOne.getBalance();
+    std::cout << "Balance: $" << temp << "\n";
     acctOne.make_deposit(30.99);
     acctOne.make_withdrawl(500);
     acctOne.print_statement();
 
     checkingAccount acctTwo;
-    acctTwo.set_name("account Two / Checking");
-    acctTwo.set_acct_num(50500020);
+    acctTwo.set_name("Service Charge / Checking");
+    acctTwo.set_acct_num(550002);
     acctTwo.set_balance(200);
-    acctTwo.set_service_charge(7.99);
+    acctTwo.set_min_balance_fee(7.99);
+    acctTwo.set_service_charge(2.99);
+    acctTwo.set_interest_rate(0);
+    temp = acctTwo.get_Check_Max();
+    std::cout <<  "Ammount of checks left this month: " << temp << ".\n";
+
+    temp = acctTwo.getBalance();
+    std::cout << "Balance: $" << temp << "\n";
+
     acctTwo.minBalance = 150;
-    acctTwo.print_statement();
     acctTwo.writeCheck(60);
     acctTwo.print_statement();
 
-    savingsAccount acctThree;
-    acctThree.set_name("Account Three / Savings");
+    checkingAccount acctThree;
+    acctThree.set_name("No Service Charge / Checking");
     acctThree.set_acct_num(550003);
-    acctThree.set_balance(600);
-    acctThree.set_interest_rate(6);
-    acctThree.calculate_interest(12);
+    acctThree.set_balance(400);
+    acctThree.set_min_balance_fee(5.99);
+    acctThree.set_service_charge(0);
+    acctThree.set_interest_rate(2);
+    acctThree.set_check_max(100);
+    temp = acctThree.get_Check_Max();
+    std::cout <<  "Ammount of checks left this month: " << temp << ".\n";
+
+    temp = acctThree.getBalance();
+    std::cout << "Balance: $" << temp << "\n";
+
+    acctThree.minBalance = 200;
+    acctThree.writeCheck(60);
     acctThree.print_statement();
 
+    savingsAccount acctFour;
+    acctFour.set_name("Savings Account");
+    acctFour.set_acct_num(550004);
+    acctFour.set_balance(600);
+    acctFour.set_interest_rate(6);
+    acctFour.set_month(8);
+    acctFour.set_min_balance(0);
+    acctFour.print_statement();
+    acctFour.make_withdrawl(50);
+    acctFour.make_deposit(150);
+    acctFour.print_statement();
 
+    savingsAccount acctFive;
+    acctFive.set_name("High interest Savings Account");
+    acctFive.set_acct_num(550005);
+    acctFive.set_balance(2500);
+    acctFive.set_interest_rate(1000);
+    acctFive.set_month(6);
+    acctFive.set_min_balance(2000);
+    acctFive.print_statement();
+    acctFive.make_withdrawl(50);
+    acctFive.make_deposit(150);
+    acctFive.print_statement();
 
+    certificateOfDeposit certOne;
+    certOne.set_name("certificate Of Deposit Account");
+    certOne.set_acct_num(550006);
+    certOne.set_balance(10000);
+    certOne.set_interest_rate(4000); //interest is calculated by dividing by 100
+    certOne.set_penalty(60);
+    certOne.set_current_month(10);
+    certOne.print_statement();
 
-
-    // std::cout << "\nwhat is the name of this account?\n";
-    // std::cin >> name;    
-
+    certOne.make_withdrawl(100);
+    
+    std::cout << "\n\nFast foward to the maturity of CD.\n\n";
+    certOne.set_current_month(12);
+    certOne.make_withdrawl(200);
+    certOne.print_statement();
 }
--- hw08.h
+++ hw08.h
@@ -43,43 +43,71 @@ class bankAccount
   }
   double get_interest_rate()
   {
-    return interestRate;
+    return interestRate / 100;
   }
+  double getBalance()
+  {
+    return balance;
+  };
+  std::string get_name()
+  {
+    return name;
+  }
+  double get_account_number()
+  {
+    return accountNumber;
+  }
+
 
   virtual double make_deposit(double deposit)
   {
     balance = balance + deposit;
+    std::cout << "making a deposit of $" << deposit <<"\n";
     return balance;
   }
 
   virtual double make_withdrawl(double withdrawl)
   {
+    std::cout << "making a withdrawl of $" << withdrawl << "\n";
     balance = balance - withdrawl;
     return balance;
   }
 
-  virtual void print_statement()
+  void print_statement()
   {
     std::cout << "***********\nACCOUNT STATEMENT\n***********\n";
-    std::cout << "The name of this ammount is: " << name << ".\n";
+    std::cout << "Account Name: " << name << ".\n";
     std::cout << "Account numer: " << accountNumber << ".\n";
-    std::cout << "Your balance is: $" << balance << ".\n***********End***********\n";
+    std::cout << "Your balance is: $" << balance << ".\n";
+    if(interestRate > 0)
+    {
+      std::cout <<  "Your interest rate is: " << interestRate << "%.\n";
+    } else
+    {
+      std::cout << "this account does not accrude interest.\n";
+    }
+    // std::cout << "************End**********\n\n";
   }
 
-  virtual double getBalance()
-  {
-    return balance;
-  };
-
-  void calculate_interest(int month)
+  virtual void calculate_interest(int month)
   {
     double interestRate = bankAccount::get_interest_rate();
-    double temp = getBalance() * (interestRate/12) * month;
+    double monthlyRate = interestRate / 12;
+    double principal = getBalance();
+    double temp = (principal * (1 + (monthlyRate * month)) - principal);
 
-    std::cout << "your interest rate is ";
-    std::cout << interestRate << ".\nIf you store your money for " << month;
-    std::cout << " months you will accumulate $" << temp << ".\n";
+    if(interestRate > 0)
+    {
+      
 
+      std::cout << "If you store your money for " << month;
+      std::cout << " months you will accumulate $" << temp << ". For a total of $";
+      std::cout << principal + temp << ".\n";
+    }
+    else
+    {
+      std::cout << "There is no interest on this account.\n";
+    }
   }
 
   private:
@@ -99,35 +127,83 @@ class checkingAccount : public bankAccount
 {
   public:
 
-  double writeCheckMax;
+  double writeCheckMax = 5;
   double monthlyServiceCharge;
   double minBalance;
   double minBalanceFee;
-  
+
+  void set_check_max(double ammount)
+  {
+    writeCheckMax = ammount;
+  }
+
+  double get_Check_Max()
+  {
+    return writeCheckMax;
+  }
 
   void writeCheck(double checkAmmount)
   {
+    std::cout << "attempting to write a check for $" << checkAmmount << ".\n";
     double balance = checkingAccount::getBalance();
     balance -= checkAmmount;
 
-    if(balance > minBalance)
+    if(writeCheckMax > 0)
     {
-      std::cout << "\nworked.\n";
-      checkingAccount::set_balance(balance);
-      // return balance;
-    } else
+      if(balance > minBalance)
+      {
+        balance -= minBalanceFee;
+        std::cout << "\n successfully wrote a check for $" << checkAmmount << "\n";
+        checkingAccount::set_balance(balance);
+      } else
+      {
+        balance -= minBalanceFee;
+        std::cout << "Balance fell below the minimum of $" << minBalance << ".\nMin balance Fee of $";
+        std::cout << minBalanceFee << " charged.\nYour new balance is $" << balance << ".\n";
+        checkingAccount::set_balance(balance);
+      }
+      writeCheckMax--;
+    } else 
     {
-      balance -= minBalanceFee;
-      std::cout << "Balance is now below minimum.\nBelow min balance Fee of $";
-      std::cout << minBalanceFee << " charged.\nYour new balance is $" << balance << ".\n";
-      checkingAccount::set_balance(balance);
-      // return balance;
+      std::cout << "You have reached your monthly check maximum\nTransaction failed\n";
     }
+  }
+
+  void set_min_balance_fee(double ammount)
+  {
+    minBalanceFee = ammount;
   }
 
   void set_service_charge(double ammount)
   {
-    minBalanceFee = ammount;
+    monthlyServiceCharge = ammount;
+  }
+
+  double get_service_charge()
+  {
+    return monthlyServiceCharge;
+  }
+
+   void print_statement()
+  {
+    bankAccount::print_statement();
+    
+    // double interestRate = get_interest_rate();
+    int checkMax = get_Check_Max();
+    if(checkMax > 50)
+    std::cout << "This acount can write unlimited checks.\n";
+    else
+    {
+    std::cout << "Available Checks this month: " << checkMax << ".\n";
+    }
+    std::cout << "This account has a balance minimum of $" << minBalance << ".\n";
+    if(monthlyServiceCharge > 0)
+    {
+      std::cout << "This account has a service charge of $" << monthlyServiceCharge << ".\n";
+    } else 
+    {
+      std::cout << "This account does not have a monthly Service charge.\n";
+    }
   }
 };
 
@@ -140,9 +216,83 @@ class certificateOfDeposit : public bankAccount
 {
   private:
   double penalty;
-  int months = 6;
+  int months = 12;
   int currentMonth;
 
+  public:
+    void set_penalty(double ammount)
+    {
+      // double principal = bankAccount::getBalance();
+      penalty = ammount / 100;
+    }
+
+    void set_months_to_mature(int matureMonths)
+    {
+      months = matureMonths;
+    }
+
+    void set_current_month(double ammount)
+    {
+      currentMonth = ammount;
+    }
+
+    double make_withdrawl(double withdrawl)
+    {
+      bankAccount::make_withdrawl(withdrawl);
+      double principal = bankAccount::getBalance();
+      double temp = principal - (principal * penalty);
+
+      if(currentMonth < months)
+      {
+        std::cout << "Penalty for early withdrawl will be " << penalty << "%. Which comes out to $";
+        std::cout << principal * penalty << ".\n";
+        principal = temp;
+        return principal;
+      } else 
+      {
+        std::cout << "Withdrew without penalty.\n";
+        temp = calculateInterest(months);
+        principal = principal + temp;
+        return principal;
+      }
+    }
+    
+    double calculateInterest(int month) 
+  {
+    double interestRate = bankAccount::get_interest_rate();
+    double monthlyRate = interestRate / 12;
+    double principal = getBalance();
+    double temp = (principal * (1 + (monthlyRate * month)) - principal);
+
+    if(interestRate > 0)
+    {
+      return temp;
+    }
+    else
+    {
+      return principal;
+    }
+  }
+
+    void print_statement()
+    {
+      bankAccount::print_statement();
+      double addedTotal = calculateInterest(months);
+
+      //penalty, maturity, current month
+      std::cout << "The penalty for early withdral onthis account is: " << penalty << "%\n";
+      std::cout << "This account will take " << months << " months to mature.\n";
+      if(currentMonth < months)
+      {
+        std::cout << "You have " << (months - currentMonth) << " months left until maturity.\n";
+      } else
+      {
+        std::cout <<"This account has matured and is ready to pay out $" << addedTotal << ".\n";
+        double total = getBalance() + addedTotal;
+        set_balance(total);
+        std::cout << "your new total is $" << getBalance() << ".\n";
+      }
+    }
 };
 
 /*
@@ -156,17 +306,32 @@ class savingsAccount : public bankAccount
   double minBalance;
   int month;
 
-
   public:
+
+  void set_month(int ammount)
+  {
+    month = ammount;
+  }
+
   void set_min_balance(double ammount)
   {
     minBalance = ammount;
   }
 
-  void print_statement() override
+  void print_statement()
   {
+    double interestRate = bankAccount::get_interest_rate();
+    double interest = bankAccount::get_interest_rate();
     bankAccount::print_statement();
-    std::cout << "Your interest rate is: " << bankAccount::get_interest_rate() << "%\n";
+    if(minBalance > 0)
+    {
+      std::cout << "Account minimum is: $" << minBalance << ".\n";
+    } else 
+    {
+      std::cout << "This account has no Minimum.\n";
+    }
+
+    bankAccount::calculate_interest(month);
   }
 
   
